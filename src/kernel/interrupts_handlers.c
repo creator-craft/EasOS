bed81fc2cd3108952d834c455a92bccc07346dd8
creator-craft <crafteur903@gmail.com>
2025-09-06 10:58:09 +0200

    fix: necked -> naked

--- src/kernel/interrupts_handlers.c
+++ src/kernel/interrupts_handlers.c
@@ -1,105 +1,105 @@
 #include "interrupts_handlers.h"
 
-#define necked __attribute__((naked)) void
+#define naked __attribute__((naked)) void
 
 static inline void iret() { __asm__ volatile ( "iret" ); }
 
 
-necked IRQ0() { iret(); }
+naked IRQ0() { iret(); }
 
-necked IRQ1() { iret(); }
+naked IRQ1() { iret(); }
 
-necked IRQ2() { iret(); }
+naked IRQ2() { iret(); }
 
-necked IRQ3() { iret(); }
+naked IRQ3() { iret(); }
 
-necked IRQ4() { iret(); }
+naked IRQ4() { iret(); }
 
-necked IRQ5() { iret(); }
+naked IRQ5() { iret(); }
 
-necked IRQ6() { iret(); }
+naked IRQ6() { iret(); }
 
-necked IRQ7() { iret(); }
+naked IRQ7() { iret(); }
 
-necked IRQ8() { iret(); }
+naked IRQ8() { iret(); }
 
-necked IRQ9() { iret(); }
+naked IRQ9() { iret(); }
 
-necked IRQ10() { iret(); }
+naked IRQ10() { iret(); }
 
-necked IRQ11() { iret(); }
+naked IRQ11() { iret(); }
 
-necked IRQ12() { iret(); }
+naked IRQ12() { iret(); }
 
-necked IRQ13() { iret(); }
+naked IRQ13() { iret(); }
 
-necked IRQ14() { iret(); }
+naked IRQ14() { iret(); }
 
-necked IRQ15() { iret(); }
+naked IRQ15() { iret(); }
 
-necked IRQ16() { iret(); }
+naked IRQ16() { iret(); }
 
-necked IRQ17() { iret(); }
+naked IRQ17() { iret(); }
 
-necked IRQ18() { iret(); }
+naked IRQ18() { iret(); }
 
-necked IRQ19() { iret(); }
+naked IRQ19() { iret(); }
 
-necked IRQ20() { iret(); }
+naked IRQ20() { iret(); }
 
-necked IRQ21() { iret(); }
+naked IRQ21() { iret(); }
 
-necked IRQ22() { iret(); }
+naked IRQ22() { iret(); }
 
-necked IRQ23() { iret(); }
+naked IRQ23() { iret(); }
 
-necked IRQ24() { iret(); }
+naked IRQ24() { iret(); }
 
-necked IRQ25() { iret(); }
+naked IRQ25() { iret(); }
 
-necked IRQ26() { iret(); }
+naked IRQ26() { iret(); }
 
-necked IRQ27() { iret(); }
+naked IRQ27() { iret(); }
 
-necked IRQ28() { iret(); }
+naked IRQ28() { iret(); }
 
-necked IRQ29() { iret(); }
+naked IRQ29() { iret(); }
 
-necked IRQ30() { iret(); }
+naked IRQ30() { iret(); }
 
-necked IRQ31() { iret(); }
+naked IRQ31() { iret(); }
 
-necked IRQ32() { iret(); }
+naked IRQ32() { iret(); }
 
-necked IRQ33() { iret(); }
+naked IRQ33() { iret(); }
 
-necked IRQ34() { iret(); }
+naked IRQ34() { iret(); }
 
-necked IRQ35() { iret(); }
+naked IRQ35() { iret(); }
 
-necked IRQ36() { iret(); }
+naked IRQ36() { iret(); }
 
-necked IRQ37() { iret(); }
+naked IRQ37() { iret(); }
 
-necked IRQ38() { iret(); }
+naked IRQ38() { iret(); }
 
-necked IRQ39() { iret(); }
+naked IRQ39() { iret(); }
 
-necked IRQ40() { iret(); }
+naked IRQ40() { iret(); }
 
-necked IRQ41() { iret(); }
+naked IRQ41() { iret(); }
 
-necked IRQ42() { iret(); }
+naked IRQ42() { iret(); }
 
-necked IRQ43() { iret(); }
+naked IRQ43() { iret(); }
 
-necked IRQ44() { iret(); }
+naked IRQ44() { iret(); }
 
-necked IRQ45() { iret(); }
+naked IRQ45() { iret(); }
 
-necked IRQ46() { iret(); }
+naked IRQ46() { iret(); }
 
-necked IRQ47() { iret(); }
+naked IRQ47() { iret(); }
 
 
 
