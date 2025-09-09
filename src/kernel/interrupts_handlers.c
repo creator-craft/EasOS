1fc9bba452db3708b3d7c3d0858c19ac785a4a07
creator-craft <crafteur903@gmail.com>
2025-09-09 19:34:41 +0200

    add: keyboard

--- src/kernel/interrupts_handlers.c
+++ src/kernel/interrupts_handlers.c
@@ -1,6 +1,45 @@
 #include "text_mode.h"
 #include "io.h"
+#include "keyboard.h"
 
 void keyboard_handler() {
-  print_char(inb(0x60));
+  u8 keycode = inb(0x60);
+
+  u8 scancode = *((u8*)keymap + 4*(keycode & 0b01111111) + key_modifiers);
+
+  if (keycode & 0b10000000) // Key released
+    switch (scancode) {
+    case KEY_LSHIFT: case KEY_RSHIFT:
+      key_modifiers &= ~SHIFT_MODIFIER;
+      break;
+    case KEY_ALT: case KEY_ALT_SYSREQ:
+      key_modifiers &= ~ALT_MODIFIER;
+      break;
+    case KEY_CTRL:
+      key_modifiers &= ~CTRL_MODIFIER;
+      break;
+
+    }
+  else // Key pressed
+    switch (scancode) {
+    case KEY_NULL: break;
+    case KEY_LSHIFT: case KEY_RSHIFT:
+      key_modifiers |= SHIFT_MODIFIER;
+      break;
+    case KEY_ALT: case KEY_ALT_SYSREQ:
+      key_modifiers |= ALT_MODIFIER;
+      break;
+    case KEY_CTRL:
+      key_modifiers |= CTRL_MODIFIER;
+      break;
+    case KEY_ENTER:
+      print_new_line();
+      break;
+    case KEY_BACKSPACE:
+      remove_char();
+      break;
+
+    default:
+      print_char(scancode);
+    }
 }
