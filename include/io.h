53dec25cb4248c14714413bc7d28244762fe0da4
creator-craft <crafteur903@gmail.com>
2025-09-10 16:06:57 +0200

    add: io for word and double word

--- include/io.h
+++ include/io.h
@@ -7,9 +7,29 @@ static inline __attribute__((always_inline)) void outb(u16 port, u8 val) {
   __asm__ volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
 }
 
+static inline __attribute__((always_inline)) void outw(u16 port, u16 val) {
+  __asm__ volatile ( "outw %0, %1" : : "a"(val), "Nd"(port) );
+}
+
+static inline __attribute__((always_inline)) void outd(u16 port, u32 val) {
+  __asm__ volatile ( "outd %0, %1" : : "a"(val), "Nd"(port) );
+}
+
 static inline __attribute__((always_inline)) u8 inb(u16 port) {
-  char ret;
+  u8 ret;
   __asm__ volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
+  return ret;
+}
+
+static inline __attribute__((always_inline)) u16 inw(u16 port) {
+  u16 ret;
+  __asm__ volatile ( "inw %1, %0" : "=a"(ret) : "Nd"(port) );
+  return ret;
+}
+
+static inline __attribute__((always_inline)) u32 ind(u16 port) {
+  u32 ret;
+  __asm__ volatile ( "ind %1, %0" : "=a"(ret) : "Nd"(port) );
   return ret;
 }
 
