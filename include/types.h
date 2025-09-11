ac59a4bb41c8fe56f667a0d23e47b50d34eb8881
creator-craft <crafteur903@gmail.com>
2025-09-11 19:27:33 +0200

    add: u64, i64 and NULL

--- include/types.h
+++ include/types.h
@@ -4,9 +4,13 @@
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
+typedef unsigned long long u64;
 
 typedef char i8;
 typedef short i16;
 typedef int i32;
+typedef long long i64;
+
+#define NULL (void*)0
 
 #endif // TYPES_H
