a6fb6fdf4e1fabaacf6deebdf6906cfb3592ab87
creator-craft <crafteur903@gmail.com>
2025-10-03 22:15:00 +0200

    fix: debug hex

--- src/kernel/debug.c
+++ src/kernel/debug.c
@@ -4,37 +4,37 @@
 
 void debug_hex_d(u32 v) {
   u8 tmp = v >> 28;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A'  -  10 : '0') + tmp);
   tmp = (v >> 24) & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A'  -  10 : '0') + tmp);
   tmp = (v >> 20) & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A'  -  10 : '0') + tmp);
   tmp = (v >> 16) & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A'  -  10 : '0') + tmp);
   tmp = (v >> 12) & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A' -  10 : '0') + tmp);
   tmp = (v >> 8) & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A' -  10 : '0') + tmp);
   tmp = (v >> 4) & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A' -  10 : '0') + tmp);
   tmp = v & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A' -  10 : '0') + tmp);
 }
 
 void debug_hex_w(u16 v) {
   u8 tmp = v >> 12;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A'  -  10 : '0') + tmp);
   tmp = (v >> 8) & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A'  -  10 : '0') + tmp);
   tmp = (v >> 4) & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A'  -  10 : '0') + tmp);
   tmp = v & 0xF;
-  outb(COM_PORT, (tmp >= 0xA ? 'A' : '0') + tmp);
+  outb(COM_PORT, (tmp >= 0xA ? 'A'  -  10 : '0') + tmp);
 }
 
 void debug_hex_b(u8 v) {
-  outb(COM_PORT, (v >= 0xA0 ? 'A' : '0') + (v >> 4));
-  outb(COM_PORT, ((v & 0xF) >= 0xA ? 'A' : '0') + (v & 0xF));
+  outb(COM_PORT, (v >= 0xA0 ? 'A'  -  10 : '0') + (v >> 4));
+  outb(COM_PORT, ((v & 0xF) >= 0xA ? 'A'  -  10 : '0') + (v & 0xF));
 }
 
 void debug(char *str) {
