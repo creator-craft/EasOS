7f8073e5155269c69f8412154a7e7ae79a9030f0
creator-craft <crafteur903@gmail.com>
2025-10-13 15:28:47 +0200

    clean: res (remove const)

--- src/kernel/resources.c
+++ src/kernel/resources.c
@@ -5,5 +5,5 @@ DECL_RES(mouse_bi)
 struct image cursor;
 
 void init_resources() {
-  cursor = (struct image) { (const u32*)RES(mouse_bi) + 1, *(u16*)RES(mouse_bi), *((u16*)(RES(mouse_bi) + 2)) };
+  cursor = (struct image) { (u32*)RES(mouse_bi) + 1, *(u16*)RES(mouse_bi), *((u16*)(RES(mouse_bi) + 2)) };
 }
